fad2d80a59233728f27ce0bff722b351c83ddc6f
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-08-13 11:53:38 +0700

    Use setDarkTheme function to change theme color

--- src/app/_components/Header/HeaderTop.tsx
+++ src/app/_components/Header/HeaderTop.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { FaCalendarDays } from "react-icons/fa6";
+import { MdSunny } from "react-icons/md";
 import { IoMoonSharp } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
@@ -6,12 +9,16 @@ import { FaTwitter } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
+import { useContext } from "react";
+import ThemeContext from "@/context/themeContext";
 
 const HeaderTop = () => {
+  const { darkTheme, setDarkTheme } = useContext(ThemeContext);
+
   return (
     <div className="container xl:max-w-[1248px] lg:max-w-[960px] md:max-w-[720px]">
       <div className="flex lg:justify-between justify-center py-[10px]">
-        <ul className="lg:flex hidden  text-[#B5B5B5] text-[13px] privacy-list">
+        <ul className="lg:flex hidden text-[#54595F] dark:text-[#B5B5B5] text-[13px] privacy-list">
           <li className="inline-flex items-center gap-2 cursor-pointer hover:text-[#ff1d50] transition-colors duration-300 ease-in-out relative">
             <FaCalendarDays className="text-[14px] text-[#ff1d50]" />
             <span className="leading-[18px] font-normal text-[13px] mt-[4px]">
@@ -31,16 +38,39 @@ const HeaderTop = () => {
             </span>
           </li>
 
-          <li className="inline-flex items-center gap-2 cursor-pointer hover:text-[#ff1d50] transition-colors duration-300 ease-in-out relative">
-            <IoMoonSharp className=" text-[14px]" />
-            <span className="leading-[18px] font-normal text-[13px] mt-[4px]">
-              DARK MODE
-            </span>
+          <li>
+            {darkTheme ? (
+              <div
+                onClick={() => {
+                  setDarkTheme(false);
+                  localStorage.removeItem("ecommerce-theme");
+                }}
+                className="inline-flex items-center gap-2 cursor-pointer hover:text-[#ff1d50] transition-colors duration-300 ease-in-out relative"
+              >
+                <IoMoonSharp className=" text-[14px]" />
+                <span className="leading-[18px] font-normal text-[13px] mt-[4px]">
+                  DARK MODE
+                </span>
+              </div>
+            ) : (
+              <div
+                onClick={() => {
+                  setDarkTheme(true);
+                  localStorage.setItem("ecommerce-theme", "true");
+                }}
+                className="inline-flex items-center gap-2 cursor-pointer hover:text-[#ff1d50] transition-colors duration-300 ease-in-out relative"
+              >
+                <MdSunny className=" text-[14px]" />
+                <span className="leading-[18px] font-normal text-[13px] mt-[4px]">
+                  LIGHT MODE
+                </span>
+              </div>
+            )}
           </li>
         </ul>
 
         <div className="flex items-center text-[13px]">
-          <div className="md:flex hidden items-center text-[#B5B5B5] gap-2 login-box ">
+          <div className="md:flex hidden items-center text-[#54595F] dark:text-[#B5B5B5] gap-2 login-box ">
             <p className="flex items-center gap-2">
               <FaRegUser className="text-[#ff1d50] text-[14px] " />{" "}
               <span className="hover:text-[#ff1d50] cursor-pointer transition-colors duration-300 ease-in-out uppercase text-[13px] mt-[4px]">
@@ -53,7 +83,7 @@ const HeaderTop = () => {
             </p>
           </div>
 
-          <ul className="flex text-[#B5B5B5] xl:gap-4 lg:gap-3 md:gap-3 gap-4 text-[16px] mt-[2px]">
+          <ul className="flex text-[#54595F] dark:text-[#B5B5B5] xl:gap-4 lg:gap-3 md:gap-3 gap-4 text-[16px] mt-[2px]">
             <li className="hover:text-[#ff1d50] cursor-pointer transition-colors duration-300 ease-in-out">
               <FaFacebookF />
             </li>
