8c1336ce1bcd1b99d4a74074230f39e73e8f67af
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-08-14 17:56:42 +0700

    Edit pathname home

---
+++ src/app/_components/Home/News/index.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import "./styles.css";
+
+import LeftSide from "./LeftSide";
+import RightSide from "./RightSide";
+
+const NewsSection = () => {
+  return (
+    <div className="py-[60px]">
+      <div className="grid grid-cols-4 gap-[24px]">
+        <div className="xl:col-span-3 col-span-4">
+          <h2 className="relative text-[30px] font-semibold inline-block mb-[33px]">
+            News
+            <span className="line"></span>
+          </h2>
+          <LeftSide />
+        </div>
+        <div className="xl:col-span-1 lg:col-span-4 col-span-4">
+          <h2 className="relative text-[30px] font-semibold inline-block mb-[33px]">
+            Latest
+            <span className="line"></span>
+          </h2>
+          <RightSide />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsSection;
