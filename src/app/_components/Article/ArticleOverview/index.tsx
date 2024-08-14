4d83b59944d0ac234c5fbe5f3e9d09f4d69a9c95
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-08-14 17:55:02 +0700

    Maked Article overview page

---
+++ src/app/_components/Article/ArticleOverview/index.tsx
@@ -0,0 +1,214 @@
+import { FaCalendarDays, FaRegUser } from "react-icons/fa6";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoArrowForward } from "react-icons/io5";
+
+const articleOverviewData = [
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+
+  {
+    tagName: "News",
+    img: "https://images.unsplash.com/photo-1531356495-75ccc754cb64?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Paddle your way to adventure, embrace kayaking.",
+    created: "29 Mar, 2023",
+    createBy: "By - Tnews ",
+  },
+];
+
+const ArticleOverView = () => {
+  return (
+    <div className="article-overview">
+      <div className="breadcrumb py-[18px] bg-[#EFEFEF]">
+        <div className="container xl:max-w-[1248px] lg:max-w-[960px] max-w-[720px]">
+          <ul className="breadcumb-menu flex gap-2">
+            <li className="inline-flex items-center gap-2 uppercase cursor-pointer font-semibold">
+              Home <IoIosArrowForward />
+            </li>
+            <li className="uppercase  font-semibold text-[#FF1D50]">
+              Video overview
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <section className="pt-[60px] pb-[30px]">
+        <div className="container xl:max-w-[1248px] lg:max-w-[960px] max-w-[720px]">
+          <div className="grid xl:grid-cols-4 lg:grid-cols-2 grid-cols-1 gap-[20px]">
+            {articleOverviewData.map((item, index) => (
+              <div key={index}>
+                <div className="mb-[25px] overflow-hidden xl:w-[280px] lg:w-[442px] w-full xl:h-[187px] lg:h-[360px] h-[400px] relative box">
+                  <img
+                    className="xl:w-[280px] lg:w-[442px] w-full xl:h-[187px] lg:h-[360px]  h-[400px] object-cover"
+                    src={item.img}
+                    alt="News"
+                  />
+
+                  {/* Overlay */}
+                  <div className="absolute top-0 xl:w-[280px] lg:w-[442px] w-full xl:h-[187px] lg:h-[360px] h-[400px] gradient-bg flex flex-col p-[20px] justify-start">
+                    <div className="lg:mb-0 mb-[15px] px-[8px] leading-[26px] text-[12px] font-bold uppercase text-white bg-[#007BFF] rounded-[3px] w-fit hover:bg-black cursor-pointer transition-colors duration-300 ease-in-out">
+                      {item.tagName}
+                    </div>
+                  </div>
+                </div>
+                <h3 className="text-[20px] leading-[1.8] font-bold mb-[15px] cursor-pointer hover:text-[#ff1d50]">
+                  {item.title}
+                </h3>
+                <div className="flex items-center gap-3 text-[#B5B5B5]">
+                  <span className="text-[14px] font-medium flex items-center gap-1 cursor-pointer hover:text-[#ff1d50] transition-colors duration-300 ease-in-out">
+                    <FaRegUser />
+                    <p className="mt-0.5">{item.createBy}</p>
+                  </span>
+
+                  <span>|</span>
+
+                  <span className="text-[14px] font-medium flex items-center gap-1 cursor-pointer hover:text-[#ff1d50] transition-colors duration-300 ease-in-out">
+                    <FaCalendarDays />
+                    <p className="mt-0.5">{item.created}</p>
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="mt-[40px] mb-[30px]">
+            <ul className="flex items-center justify-center gap-[10px]">
+              <li className="w-[50px] h-[50px] border border-1 rounded-full flex items-center justify-center">
+                <p className=" text-[14px] font-semibold">1</p>
+              </li>
+              <li className="w-[50px] h-[50px] border border-1 rounded-full flex items-center justify-center">
+                <p className=" text-[14px] font-semibold">2</p>
+              </li>
+              <li className="w-[50px] h-[50px] border border-1 rounded-full flex items-center justify-center">
+                <p className=" text-[14px] font-semibold">3</p>
+              </li>
+              <li className="w-[50px] h-[50px] border border-1 rounded-full flex items-center justify-center">
+                <p className=" text-[14px] font-semibold">
+                  <IoArrowForward />
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default ArticleOverView;
