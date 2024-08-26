3ad52b40bbd44c7db0efcbf7c2773553b26cd7f1
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-08-26 15:23:10 +0700

    Update ui footer

--- src/app/_components/Footer/Footer.tsx
+++ src/app/_components/Footer/Footer.tsx
@@ -1,29 +1,89 @@
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaInstagram,
+  FaYoutube,
+} from "react-icons/fa";
+
 export default function Footer() {
   return (
-    <div className="p-12 pb-6 bg-black dark:bg-[#282828]">
+    <div className="pt-12 pb-6 bg-black dark:bg-[#282828]">
       <div className="container xl:max-w-[1248px] lg:max-w-[960px] md:max-w-[720px]">
         <div className="flex flex-col justify-center items-center gap-10">
-          <div>
+          <a href="/#">
             <img src="/logo.png" alt="logo" className="w-[346px] h-[114px]" />
+          </a>
+
+          <div className="flex items-center gap-[10px] flex-wrap">
+            <div className="icon-box p-[14px] rounded-full bg-[#363739] cursor-pointer">
+              <FaFacebookF className="text-white" />
+            </div>
+
+            <div className="icon-box p-[14px] rounded-full bg-[#363739] cursor-pointer">
+              <FaTwitter className="text-white" />
+            </div>
+
+            <div className="icon-box p-[14px] rounded-full bg-[#363739] cursor-pointer">
+              <FaLinkedinIn className="text-white" />
+            </div>
+
+            <div className="icon-box p-[14px] rounded-full bg-[#363739] cursor-pointer">
+              <FaInstagram className="text-white" />
+            </div>
+
+            <div className="icon-box p-[14px] rounded-full bg-[#363739] cursor-pointer">
+              <FaYoutube className="text-white" />
+            </div>
           </div>
 
-          <p className="text-white  text-center">
-            "Vielmehr seid ihr zu dem Berg Zion gekommen und der Stadt des
-            lebendigen Gottes, dem himmlischen Jerusalem ..." (Hebr. 12:22). Die
-            Gemeinde, in der unser Gott und Vater angebetet werden möchte, ist
-            nicht nur eine Versammlung von Christen mit einem bestimmten Namen.
-            Wir haben den Wunsch, heute das himmlische Jerusalem zu sein und
-            dort unseren Vater-Gott im Geist und in der Wirklichkeit anzubeten.
-            Alle, die diesen Wunsch mit uns teilen, sind herzlich eingeladen!
-          </p>
+          <nav className="flex items-center gap-[30px] text-white flex-wrap justify-center">
+            <a href="/#" className="uppercase">
+              home
+            </a>
+            <a href="/#" className="uppercase">
+              features
+            </a>
+            <a href="/#" className="uppercase">
+              category
+            </a>
+            <a href="/#" className="uppercase">
+              posts
+            </a>
+            <a href="/#" className="uppercase">
+              gadgets
+            </a>
+            <a href="/#" className="uppercase">
+              contact
+            </a>
+          </nav>
 
-          <div className="flex md:flex-row flex-col md:gap-0 gap-6 items-center justify-between text-white flex-grow w-full">
-            <p className="text-center">© Copyright 2024 </p>
-            <div>
-              <ul className="flex md:flex-row flex-col gap-6 justify-center items-center">
-                <li>Imprint</li>
-                <li>Data protection</li>
-                <li>Cookies</li>
+          <div className="flex lg:flex-row flex-col lg:gap-0 gap-6 items-center justify-between text-[#838f92] flex-grow w-full text-[12px]">
+            <p className="text-center lg:order-1 order-2">
+              Copyright © 2024 HIMMLISCHES JERUSALEM. All rights reserved.
+            </p>
+            <div className="lg:order-2 order-1">
+              <ul className="flex sm:flex-row flex-col items-center gap-y-2">
+                <li className="capitalize">
+                  <a href="/#" className="px-[16px]">
+                    Home
+                  </a>
+                </li>
+                <li className="capitalize">
+                  <a href="/#" className="px-[16px]">
+                    About Us
+                  </a>
+                </li>
+                <li className="capitalize">
+                  <a href="/#" className="px-[16px]">
+                    Faq
+                  </a>
+                </li>
+                <li className="capitalize">
+                  <a href="/#" className="px-[16px]">
+                    About Us
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
