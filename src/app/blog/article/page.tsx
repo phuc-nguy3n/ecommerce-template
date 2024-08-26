f140dd119c2cebc194c8734b0b7df8c9e68d3783
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-08-26 18:04:58 +0700

    Setting url pages blog video, article

---
+++ src/app/blog/article/page.tsx
@@ -0,0 +1,7 @@
+import BlogArticleDetails from "@/app/_components/Blog/BlogDetails/BlogArticle";
+
+const BlogArticle = () => {
+  return <BlogArticleDetails />;
+};
+
+export default BlogArticle;
---
+++ src/app/blog/video/page.tsx
@@ -0,0 +1,7 @@
+import BlogVideoDetails from "@/app/_components/Blog/BlogDetails/BlogVideo";
+
+const BlogVideo = () => {
+  return <BlogVideoDetails />;
+};
+
+export default BlogVideo;
