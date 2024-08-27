7302467ee96c04fa5603627581dc63e2269bf4a4
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-08-27 18:52:11 +0700

    Revert "Created PostImage component"
    This reverts commit 712bec5f767d3fdffd4d5ec142225e2516a340ae.

--- src/app/_components/Home/Articles/index.tsx
+++ src/app/_components/Home/Articles/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { FaCalendarDays, FaRegUser } from "react-icons/fa6";
 import "../styles.css";
-import PostImage from "../../ui/PostImage";
 
 const articlePost = [
   {
@@ -28,50 +27,41 @@ export default function ArticlesSection() {
 
       <div className="grid xl:grid-cols-3 grid-cols-2 gap-5">
         <div className="xl:col-span-2 md:col-span-1 col-span-2">
-          <PostImage
-            width={{
-              xl: "full",
-              lg: "full",
-              md: "full",
-              sm: "full",
-              xs: "full",
-            }}
-            height={{
-              xl: "[430px]",
-              lg: "[430px]",
-              md: "[430px]",
-              sm: "[430px]",
-              xs: "[430px]",
-            }}
-            padding={{
-              xl: "[30px]",
-              lg: "[30px]",
-              md: "[30px]",
-              sm: "[30px]",
-              xs: "[20px]",
-            }}
-            titleSize={{
-              xl: "[24px]",
-              lg: "[24px]",
-              md: "[24px]",
-              sm: "[24px]",
-              xs: "[18px]",
-            }}
-            colorTag={"[#8750A6]"}
-            tagPosition={"bottom"}
-            tagName={"Article"}
-            cover={
-              "https://i1-english.vnecdn.net/2024/08/08/USDollarbanknotesareseeninthis-4502-6611-1723090850.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=7e9-AFqpheXxYawyLRiMew"
-            }
-            title={"Your life, upgraded - gadgets Make it extraordinary"}
-            author={"By - Tnews"}
-            created={"20 Mar, 2023"}
-          />
+          <div className="h-[430px] overflow-hidden relative box">
+            <img
+              className="h-full w-full object-cover transform transition-transform duration-500 hover:scale-105"
+              src="https://i1-english.vnecdn.net/2024/08/08/USDollarbanknotesareseeninthis-4502-6611-1723090850.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=7e9-AFqpheXxYawyLRiMew"
+              alt="Article"
+            />
+
+            <div className="absolute top-0 w-full h-[430px] gradient-bg home flex gap-2 flex-col md:p-[30px] p-[20px]">
+              <div className="px-[8px] leading-[26px] text-[12px] font-bold uppercase  text-white bg-[#8750A6] rounded-[3px]  w-fit hover:bg-black cursor-pointer transition-colors duration-300 ease-in-out">
+                Article
+              </div>
+              <h3 className="text-white md:text-[24px] text-[18px] font-semibold hover:text-[#ff1d50] cursor-pointer transition-colors duration-300 ease-in-out">
+                Your life, upgraded - gadgets Make it extraordinary
+              </h3>
+
+              <div className="flex items-center gap-3 text-[#B5B5B5]">
+                <span className="text-[14px] font-semibold flex items-center gap-1 cursor-pointer hover:text-[#ff1d50] transition-colors duration-300 ease-in-out">
+                  <FaRegUser />
+                  <p className="mt-0.5">By - Tnews</p>
+                </span>
+
+                <span>|</span>
+
+                <span className="text-[14px] font-semibold flex items-center gap-1 cursor-pointer hover:text-[#ff1d50] transition-colors duration-300 ease-in-out">
+                  <FaCalendarDays />
+                  <p className="mt-0.5">20 Mar, 2023 </p>
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="xl:col-span-1 md:col-span-1 col-span-2 flex flex-col gap-5">
           {articlePost.map((item, index) => (
             <div className="flex gap-3" key={index}>
-              <div className="md:w-[250px] w-[130px] h-[130px] overflow-hidden">
+              <div className="md:w-[250px] w-[130px]  h-[130px] overflow-hidden">
                 <img
                   className="object-cover w-full h-full transform transition-transform duration-500 hover:scale-105"
                   src={item.img}
