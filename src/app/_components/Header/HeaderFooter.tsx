34d219f8c1df1b6cb4852ba116547319d23465b3
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-08-28 11:47:43 +0700

    Break down into smaller components at HeaderFooter

--- src/app/_components/Header/HeaderFooter.tsx
+++ src/app/_components/Header/HeaderFooter.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
 import { IoSearch } from "react-icons/io5";
 import { GrCart } from "react-icons/gr";
@@ -8,20 +8,31 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import MenuMobile from "../MenuMobile";
 import Link from "next/link";
 
-export const subnavData = {
-  blog: [
-    {
-      name: "Blog details video",
-      url: "/blog/video",
-    },
-    {
-      name: "Blog details article ",
-      url: "/blog/article",
-    },
-  ],
+type SubNavbarItemType = {
+  name: string;
+  url: string;
 };
 
-export const navbar = [
+export type NavbarItemType = {
+  name: string;
+  url: string;
+  submenu: SubNavbarItemType[];
+};
+
+type NavbarItemComponentProps = {
+  item: NavbarItemType;
+  index: number;
+};
+
+type LogoComponentProps = {
+  imgURL: string;
+};
+
+type MenuBoxComponentProps = {
+  setOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+export const navbar: NavbarItemType[] = [
   {
     name: "home",
     url: "/",
@@ -97,78 +108,115 @@ const HeaderFooter = () => {
       >
         <div className="container xl:max-w-[1248px] lg:max-w-[960px] md:max-w-[720px] flex justify-between">
           <ul className="lg:flex hidden items-center gap-10">
-            {navbar.map((item: any, index: any) => (
-              <li
-                key={index}
-                className="group py-[17px] text-[14px] font-medium text-white hover:text-[#ff1d50] cursor-pointer transition-colors duration-300 ease-in-out"
-              >
-                <Link
-                  className="inline-flex items-center gap-1"
-                  href={item.url}
-                >
-                  <span className="uppercase">{item.name}</span>{" "}
-                  {item.submenu.length > 0 ? (
-                    <IoIosArrowDown className="arrow-icon" />
-                  ) : (
-                    ""
-                  )}
-                </Link>
-
-                {item.submenu.length > 0 ? (
-                  <div className="relative subnav-block">
-                    <ul className="w-[230px] subnav absolute top-[18px] left-[-18px] p-[18px] pr-[20px] border-t-[3px] border-[#ff1d50] text-[#080809] dark:text-white text-[14px] bg-[#fff] dark:bg-[#282828]">
-                      {item.submenu.map((item: any, index: any) => (
-                        <li
-                          className="hover:text-[#ff1d50] cursor-pointer transition-colors duration-300 ease-in-out"
-                          key={index}
-                        >
-                          <Link href={item.url}>
-                            <p className="flex items-center gap-1 uppercase leading-[30px] text-nowrap">
-                              <IoIosArrowForward className="icon-subnav" />{" "}
-                              {item.name}
-                            </p>
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                ) : (
-                  ""
-                )}
-              </li>
+            {navbar.map((item: NavbarItemType, index: number) => (
+              <NavbarItem item={item} index={index} />
             ))}
           </ul>
 
-          <div className="md:flex lg:hidden justify-center py-[10px]">
-            <img
-              src="/logo.png"
-              alt="logo"
-              className="cursor-pointer w-[143px] h-[60px]"
-            />
-          </div>
+          <Logo imgURL={"/logo.png"} />
 
           <div className="flex items-center lg:gap-10 gap-[20px]">
             <div className="flex gap-4 text-white">
-              <div className="search-box hover:text-[#ff1d50] cursor-pointer transition-colors duration-300 ease-in-out">
-                <IoSearch className="text-[26px]" />
-              </div>
-              <div className="cart-box hover:text-[#ff1d50] cursor-pointer transition-colors duration-300 ease-in-out">
-                <GrCart className="text-[24px]" />
-              </div>
+              <SearchBox />
+              <CartBox />
             </div>
 
-            <div
-              className="menu-box cursor-pointer bg-[#ff1d50] p-[17px] text-white lg:hidden md:block"
-              onClick={() => setOpenMenu(true)}
-            >
-              <RxHamburgerMenu className="text-[24px] " />
-            </div>
+            <MenuBox setOpen={setOpenMenu} />
           </div>
         </div>
       </nav>
 
       <MenuMobile isOpen={isOpenMenu} setOpen={setOpenMenu} />
     </>
+  );
+};
+
+const NavbarItem: React.FC<NavbarItemComponentProps> = ({ item, index }) => {
+  return (
+    <li
+      key={index}
+      className="group py-[17px] text-[14px] font-medium text-white hover:text-[#ff1d50] cursor-pointer transition-colors duration-300 ease-in-out"
+    >
+      {item.url ? (
+        <Link className="inline-flex items-center gap-1" href={item.url}>
+          <span className="uppercase">{item.name}</span>{" "}
+          {item.submenu.length > 0 ? (
+            <IoIosArrowDown className="arrow-icon" />
+          ) : (
+            ""
+          )}
+        </Link>
+      ) : (
+        <div className="inline-flex items-center gap-1">
+          <span className="uppercase">{item.name}</span>{" "}
+          {item.submenu.length > 0 ? (
+            <IoIosArrowDown className="arrow-icon" />
+          ) : (
+            ""
+          )}
+        </div>
+      )}
+
+      {item.submenu.length > 0 ? (
+        <div className="relative subnav-block">
+          <ul className="w-[230px] subnav absolute top-[18px] left-[-18px] p-[18px] pr-[20px] border-t-[3px] border-[#ff1d50] text-[#080809] dark:text-white text-[14px] bg-[#fff] dark:bg-[#282828]">
+            {item.submenu.map((item: any, index: any) => (
+              <li
+                className="hover:text-[#ff1d50] cursor-pointer transition-colors duration-300 ease-in-out"
+                key={index}
+              >
+                <Link href={item.url}>
+                  <p className="flex items-center gap-1 uppercase leading-[30px] text-nowrap">
+                    <IoIosArrowForward className="icon-subnav" /> {item.name}
+                  </p>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        ""
+      )}
+    </li>
+  );
+};
+
+const Logo: React.FC<LogoComponentProps> = ({ imgURL }) => {
+  return (
+    <div className="md:flex lg:hidden justify-center py-[10px]">
+      <img
+        src={imgURL}
+        alt="logo"
+        className="cursor-pointer w-[143px] h-[60px]"
+      />
+    </div>
+  );
+};
+
+const SearchBox = () => {
+  return (
+    <div className="search-box hover:text-[#ff1d50] cursor-pointer transition-colors duration-300 ease-in-out">
+      <IoSearch className="text-[26px]" />
+    </div>
+  );
+};
+
+const CartBox = () => {
+  return (
+    <div className="cart-box hover:text-[#ff1d50] cursor-pointer transition-colors duration-300 ease-in-out">
+      <GrCart className="text-[24px]" />
+    </div>
+  );
+};
+
+const MenuBox: React.FC<MenuBoxComponentProps> = ({ setOpen }) => {
+  return (
+    <div
+      className="menu-box cursor-pointer bg-[#ff1d50] p-[17px] text-white lg:hidden md:block"
+      onClick={() => setOpen(true)}
+    >
+      <RxHamburgerMenu className="text-[24px] " />
+    </div>
   );
 };
 
