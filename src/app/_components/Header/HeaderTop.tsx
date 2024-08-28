d404117004afbb422bc4fe783c100a0d07f8e689
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-08-28 13:53:11 +0700

    Move style

--- src/app/_components/Header/HeaderTop.tsx
+++ src/app/_components/Header/HeaderTop.tsx
@@ -16,6 +16,23 @@ import {
   FaYoutube,
 } from "react-icons/fa";
 
+const style = {
+  flexBox: "flex lg:justify-between justify-center py-[10px]",
+  navLeft:
+    "lg:flex hidden text-[--text-content-light] dark:text-[--text-content-dark] text-[13px] privacy-list",
+  navItem:
+    "inline-flex items-center gap-2 cursor-pointer hover:text-[--primary] transition-colors duration-300 ease-in-out relative",
+  text: "leading-[18px] font-normal text-[13px] mt-[4px]",
+  navRight:
+    "flex items-center text-[13px] text-[--text-content-light] dark:text-[--text-content-dark]",
+  loginBox: "md:flex hidden items-center gap-2 login-box",
+  textLogin:
+    "hover:text-[--primary] cursor-pointer transition-colors duration-300 ease-in-out uppercase text-[13px] mt-[4px]",
+  iconBox:
+    "flex text-[--text-content-light] dark:text-[--text-content-dark] xl:gap-4 lg:gap-3 md:gap-3 gap-4 text-[16px] mt-[2px]",
+  icon: "hover:text-[--primary] cursor-pointer transition-colors duration-300 ease-in-out",
+};
+
 const iconList = [
   {
     url: "",
@@ -40,23 +57,6 @@ const iconList = [
 ];
 
 const HeaderTop = () => {
-  const style = {
-    flexBox: "flex lg:justify-between justify-center py-[10px]",
-    navLeft:
-      "lg:flex hidden text-[--text-content-light] dark:text-[--text-content-dark] text-[13px] privacy-list",
-    navItem:
-      "inline-flex items-center gap-2 cursor-pointer hover:text-[--primary] transition-colors duration-300 ease-in-out relative",
-    text: "leading-[18px] font-normal text-[13px] mt-[4px]",
-    navRight:
-      "flex items-center text-[13px] text-[--text-content-light] dark:text-[--text-content-dark]",
-    loginBox: "md:flex hidden items-center gap-2 login-box",
-    textLogin:
-      "hover:text-[--primary] cursor-pointer transition-colors duration-300 ease-in-out uppercase text-[13px] mt-[4px]",
-    iconBox:
-      "flex text-[--text-content-light] dark:text-[--text-content-dark] xl:gap-4 lg:gap-3 md:gap-3 gap-4 text-[16px] mt-[2px]",
-    icon: "hover:text-[--primary] cursor-pointer transition-colors duration-300 ease-in-out",
-  };
-
   const { darkTheme, setDarkTheme } = useContext(ThemeContext);
 
   return (
