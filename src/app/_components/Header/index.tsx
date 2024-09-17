53c9e9511daa6f9436fc8385e4ca5cbdac492bbf
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-09-17 12:24:02 +0700

    Making sticky nav

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -233,99 +233,261 @@ const Header = () => {
     miniCart!.classList.remove("visible-custom");
   };
 
-  const handleScroll = () => {
-    const targetElement = document.querySelector("#header-bottom");
-
-    if (window.scrollY > 500) {
-      targetElement!.classList.add("header-visible");
-    } else {
-      targetElement!.classList.remove("header-visible");
-    }
-  };
-
-  window.addEventListener("scroll", handleScroll);
-
   return (
-    <header className={headerBgColor}>
-      {/* Header top */}
-      <Container>
-        <div className={headerTopStyle.flexBox}>
-          <ul className={headerTopStyle.navLeft}>
-            <li className={headerTopStyle.navItem}>
-              <FaCalendarDays className="text-[14px] text-[--primary]" />
-              <span className={headerTopStyle.text}>20 AUGUST, 2023</span>
-            </li>
+    <>
+      <header className={headerBgColor}>
+        {/* Header top */}
+        <Container>
+          <div className={headerTopStyle.flexBox}>
+            <ul className={headerTopStyle.navLeft}>
+              <li className={headerTopStyle.navItem}>
+                <FaCalendarDays className="text-[14px] text-[--primary]" />
+                <span className={headerTopStyle.text}>20 AUGUST, 2023</span>
+              </li>
 
-            <li className={headerTopStyle.navItem}>
-              <span className={headerTopStyle.text}>PRIVACY POLICY</span>
-            </li>
+              <li className={headerTopStyle.navItem}>
+                <span className={headerTopStyle.text}>PRIVACY POLICY</span>
+              </li>
 
-            <li className={headerTopStyle.navItem}>
-              <span className={headerTopStyle.text}>TERMS & CONDITIONS</span>
-            </li>
+              <li className={headerTopStyle.navItem}>
+                <span className={headerTopStyle.text}>TERMS & CONDITIONS</span>
+              </li>
 
-            <li>
-              {darkTheme ? (
-                <div
-                  onClick={() => {
-                    setDarkTheme(false);
-                    localStorage.removeItem("ecommerce-theme");
-                  }}
-                  className={headerTopStyle.navItem}
-                >
-                  <IoMoonSharp className=" text-[14px]" />
-                  <span className={headerTopStyle.text}>DARK MODE</span>
-                </div>
-              ) : (
-                <div
-                  onClick={() => {
-                    setDarkTheme(true);
-                    localStorage.setItem("ecommerce-theme", "true");
-                  }}
-                  className={headerTopStyle.navItem}
-                >
-                  <MdSunny className="text-[14px]" />
-                  <span className={headerTopStyle.text}>LIGHT MODE</span>
-                </div>
-              )}
-            </li>
-          </ul>
-
-          <div className={headerTopStyle.navRight}>
-            <div className={headerTopStyle.loginBox}>
-              <Link href="/login" className="flex items-center gap-2">
-                <FaRegUser className="text-[--primary] text-[14px]" />{" "}
-                <span className={headerTopStyle.textLogin}>Login</span>
-              </Link>
-              <span className="mt-[4px]">/</span>{" "}
-              <p className={headerTopStyle.textLogin}>Register</p>
-            </div>
-
-            <ul className={headerTopStyle.iconBox}>
-              {iconList.map((item, index) => (
-                <li key={index} className={headerTopStyle.icon}>
-                  {item.icon}
-                </li>
-              ))}
+              <li>
+                {darkTheme ? (
+                  <div
+                    onClick={() => {
+                      setDarkTheme(false);
+                      localStorage.removeItem("ecommerce-theme");
+                    }}
+                    className={headerTopStyle.navItem}
+                  >
+                    <IoMoonSharp className=" text-[14px]" />
+                    <span className={headerTopStyle.text}>DARK MODE</span>
+                  </div>
+                ) : (
+                  <div
+                    onClick={() => {
+                      setDarkTheme(true);
+                      localStorage.setItem("ecommerce-theme", "true");
+                    }}
+                    className={headerTopStyle.navItem}
+                  >
+                    <MdSunny className="text-[14px]" />
+                    <span className={headerTopStyle.text}>LIGHT MODE</span>
+                  </div>
+                )}
+              </li>
             </ul>
+
+            <div className={headerTopStyle.navRight}>
+              <div className={headerTopStyle.loginBox}>
+                <Link href="/login" className="flex items-center gap-2">
+                  <FaRegUser className="text-[--primary] text-[14px]" />{" "}
+                  <span className={headerTopStyle.textLogin}>Login</span>
+                </Link>
+                <span className="mt-[4px]">/</span>{" "}
+                <p className={headerTopStyle.textLogin}>Register</p>
+              </div>
+
+              <ul className={headerTopStyle.iconBox}>
+                {iconList.map((item, index) => (
+                  <li key={index} className={headerTopStyle.icon}>
+                    {item.icon}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </Container>
+
+        {/* Header middle */}
+        <div className={headerMiddleStyle.logoBox}>
+          <Link href="/">
+            <img src="/logo.png" alt="logo" className={headerMiddleStyle.img} />
+          </Link>
+
+          <div className={headerMiddleStyle.adsBox}>
+            <h1 className="h-[60px] bg-[#f3f4f6] font-bold text-[34px] flex items-center justify-center">
+              700 x 60
+            </h1>
           </div>
         </div>
-      </Container>
 
-      {/* Header middle */}
-      <div className={headerMiddleStyle.logoBox}>
-        <Link href="/">
-          <img src="/logo.png" alt="logo" className={headerMiddleStyle.img} />
-        </Link>
+        {/* Search Box */}
+        <div
+          className={` ${searchPopupStyle.overlay} ${
+            isSearchPopupOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+          }`}
+        >
+          <div className={searchPopupStyle.wrapper}>
+            <button
+              onClick={() => setIsSearchPopupOpen(false)}
+              className={searchPopupStyle.btnClose}
+            >
+              <IoCloseOutline className="text-[--primary] text-3xl hover:text-black" />
+            </button>
 
-        <div className={headerMiddleStyle.adsBox}>
-          <h1 className="h-[60px] bg-[#f3f4f6] font-bold text-[34px] flex items-center justify-center">
-            700 x 60
-          </h1>
+            <div className="relative">
+              <input
+                type="text"
+                className={searchPopupStyle.input}
+                placeholder="What are you looking for?"
+              />
+
+              <button type="submit" className={searchPopupStyle.btnSubmit}>
+                <IoSearch className="text-white md:text-3xl text-2xl" />
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
 
-      {/* <HeaderBottom /> */}
+        {/* Mini cart */}
+        <div id="mini-cart" className={miniCartStyle.overlay}>
+          <div className="flex justify-end h-full">
+            <div className={miniCartStyle.wrapper}>
+              <div className="flex justify-end my-4">
+                <button
+                  onClick={closeMiniCart}
+                  className={miniCartStyle.btnClose}
+                >
+                  <IoCloseOutline className="text-3xl " />
+                </button>
+              </div>
+
+              <h1 className="text-lg font-bold mb-8">Shopping cart</h1>
+
+              <ul className="cart-list mb-4">
+                {itemsCart.length === 0 ? (
+                  <span> Cart empty</span>
+                ) : (
+                  itemsCart.map((item, index) => (
+                    <li key={index} className={miniCartStyle.itemBox}>
+                      <div className="flex justify-between gap-2">
+                        <div className={miniCartStyle.imgBox}>
+                          <img
+                            className="object-cover w-full h-full"
+                            src={item.img}
+                            alt=""
+                          />
+                        </div>
+
+                        <div className={miniCartStyle.infoBox}>
+                          <h2 className={miniCartStyle.title}>{item.name}</h2>
+                          <div className={miniCartStyle.amount}>
+                            <span className="quantity">{item.quantity}</span> x{" "}
+                            <span className="price">${item.price}</span>
+                          </div>
+                        </div>
+
+                        <div className="flex flex-col justify-center">
+                          <button onClick={() => handleRemoveItem(item.id)}>
+                            <IoCloseOutline className="text-2xl text-[--text-content]" />
+                          </button>
+                        </div>
+                      </div>
+                    </li>
+                  ))
+                )}
+              </ul>
+
+              {itemsCart.length !== 0 && (
+                <>
+                  <h3 className="mb-4 md:text-left text-right">
+                    <span className="text-base font-bold mr-1">Subtotal: </span>{" "}
+                    <span className="text-lg">$4398.00</span>
+                  </h3>
+
+                  <div className="flex gap-3 flex-wrap">
+                    <button className={miniCartStyle.btnAction}>
+                      View cart
+                    </button>
+
+                    <button className={miniCartStyle.btnAction}>
+                      Checkout
+                    </button>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+
+        {/* Menu */}
+        <div id="menu" className={menuStyle.overlay}>
+          <div className={menuStyle.menuWrapper}>
+            {/* Close button */}
+            <button className={menuStyle.closeBox} onClick={closeMenu}>
+              <IoCloseOutline className="flex-grow text-[24px]" />
+            </button>
+
+            {/* Logo */}
+            <Link href="/" onClick={closeMenu} className={menuStyle.logoBox}>
+              <img src="/logo.png" alt="logo" className="w-[143px] h-[60px]" />
+            </Link>
+
+            {/* Menu list */}
+            <div className={menuStyle.menuBody}>
+              <ul id="nav-menu" className="nav-menu px-[40px]">
+                {navbar.map((item, index) => (
+                  // Item menu
+                  <li key={index}>
+                    <div className={menuStyle.itemMenu}>
+                      {item.submenu.length > 0 ? (
+                        <>
+                          <span
+                            className={`${menuStyle.linkItemMenu} 
+                          
+                          `}
+                            onClick={() => toggleSubmenu(index)}
+                          >
+                            <IoIosArrowForward /> {item.name}
+                          </span>
+                          <button
+                            id={`toggle-submenu-${index}`}
+                            className="submenu-btn text-[18px] bg-[#F5F5F5] px-2 rounded-full"
+                            onClick={() => toggleSubmenu(index)}
+                          >
+                            +
+                          </button>
+                        </>
+                      ) : (
+                        <Link
+                          href={item.url}
+                          onClick={closeMenu}
+                          className={menuStyle.linkItemMenu}
+                        >
+                          <IoIosArrowForward /> {item.name}
+                        </Link>
+                      )}
+                    </div>
+
+                    {/* Submenu */}
+                    {item.submenu.length > 0 && (
+                      <ul id="subnav-menu" className={`subnav-menu `}>
+                        {item.submenu.map((subItem, subIndex) => (
+                          // Item submenu
+                          <li className="pl-[20px]" key={subIndex}>
+                            <Link
+                              href={subItem.url}
+                              onClick={closeMenu}
+                              className={menuStyle.linkItemSubMenu}
+                            >
+                              <IoIosArrowForward /> {subItem.name}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      {/* Sticky nav */}
       <nav id="header-bottom" className="bg-black sticky-header">
         <div className={headerBottomStyle.container}>
           {/* Navbar */}
@@ -417,175 +579,7 @@ const Header = () => {
           </div>
         </div>
       </nav>
-
-      {/* Search Box */}
-      <div
-        className={` ${searchPopupStyle.overlay} ${
-          isSearchPopupOpen ? "opacity-100" : "opacity-0 pointer-events-none"
-        }`}
-      >
-        <div className={searchPopupStyle.wrapper}>
-          <button
-            onClick={() => setIsSearchPopupOpen(false)}
-            className={searchPopupStyle.btnClose}
-          >
-            <IoCloseOutline className="text-[--primary] text-3xl hover:text-black" />
-          </button>
-
-          <div className="relative">
-            <input
-              type="text"
-              className={searchPopupStyle.input}
-              placeholder="What are you looking for?"
-            />
-
-            <button type="submit" className={searchPopupStyle.btnSubmit}>
-              <IoSearch className="text-white md:text-3xl text-2xl" />
-            </button>
-          </div>
-        </div>
-      </div>
-
-      {/* Mini cart */}
-      <div id="mini-cart" className={miniCartStyle.overlay}>
-        <div className="flex justify-end h-full">
-          <div className={miniCartStyle.wrapper}>
-            <div className="flex justify-end my-4">
-              <button
-                onClick={closeMiniCart}
-                className={miniCartStyle.btnClose}
-              >
-                <IoCloseOutline className="text-3xl " />
-              </button>
-            </div>
-
-            <h1 className="text-lg font-bold mb-8">Shopping cart</h1>
-
-            <ul className="cart-list mb-4">
-              {itemsCart.length === 0 ? (
-                <span> Cart empty</span>
-              ) : (
-                itemsCart.map((item, index) => (
-                  <li key={index} className={miniCartStyle.itemBox}>
-                    <div className="flex justify-between gap-2">
-                      <div className={miniCartStyle.imgBox}>
-                        <img
-                          className="object-cover w-full h-full"
-                          src={item.img}
-                          alt=""
-                        />
-                      </div>
-
-                      <div className={miniCartStyle.infoBox}>
-                        <h2 className={miniCartStyle.title}>{item.name}</h2>
-                        <div className={miniCartStyle.amount}>
-                          <span className="quantity">{item.quantity}</span> x{" "}
-                          <span className="price">${item.price}</span>
-                        </div>
-                      </div>
-
-                      <div className="flex flex-col justify-center">
-                        <button onClick={() => handleRemoveItem(item.id)}>
-                          <IoCloseOutline className="text-2xl text-[--text-content]" />
-                        </button>
-                      </div>
-                    </div>
-                  </li>
-                ))
-              )}
-            </ul>
-
-            {itemsCart.length !== 0 && (
-              <>
-                <h3 className="mb-4 md:text-left text-right">
-                  <span className="text-base font-bold mr-1">Subtotal: </span>{" "}
-                  <span className="text-lg">$4398.00</span>
-                </h3>
-
-                <div className="flex gap-3 flex-wrap">
-                  <button className={miniCartStyle.btnAction}>View cart</button>
-
-                  <button className={miniCartStyle.btnAction}>Checkout</button>
-                </div>
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-
-      {/* Menu */}
-      <div id="menu" className={menuStyle.overlay}>
-        <div className={menuStyle.menuWrapper}>
-          {/* Close button */}
-          <button className={menuStyle.closeBox} onClick={closeMenu}>
-            <IoCloseOutline className="flex-grow text-[24px]" />
-          </button>
-
-          {/* Logo */}
-          <Link href="/" onClick={closeMenu} className={menuStyle.logoBox}>
-            <img src="/logo.png" alt="logo" className="w-[143px] h-[60px]" />
-          </Link>
-
-          {/* Menu list */}
-          <div className={menuStyle.menuBody}>
-            <ul id="nav-menu" className="nav-menu px-[40px]">
-              {navbar.map((item, index) => (
-                // Item menu
-                <li key={index}>
-                  <div className={menuStyle.itemMenu}>
-                    {item.submenu.length > 0 ? (
-                      <>
-                        <span
-                          className={`${menuStyle.linkItemMenu} 
-                          
-                          `}
-                          onClick={() => toggleSubmenu(index)}
-                        >
-                          <IoIosArrowForward /> {item.name}
-                        </span>
-                        <button
-                          id={`toggle-submenu-${index}`}
-                          className="submenu-btn text-[18px] bg-[#F5F5F5] px-2 rounded-full"
-                          onClick={() => toggleSubmenu(index)}
-                        >
-                          +
-                        </button>
-                      </>
-                    ) : (
-                      <Link
-                        href={item.url}
-                        onClick={closeMenu}
-                        className={menuStyle.linkItemMenu}
-                      >
-                        <IoIosArrowForward /> {item.name}
-                      </Link>
-                    )}
-                  </div>
-
-                  {/* Submenu */}
-                  {item.submenu.length > 0 && (
-                    <ul id="subnav-menu" className={`subnav-menu `}>
-                      {item.submenu.map((subItem, subIndex) => (
-                        // Item submenu
-                        <li className="pl-[20px]" key={subIndex}>
-                          <Link
-                            href={subItem.url}
-                            onClick={closeMenu}
-                            className={menuStyle.linkItemSubMenu}
-                          >
-                            <IoIosArrowForward /> {subItem.name}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </header>
+    </>
   );
 };
 
